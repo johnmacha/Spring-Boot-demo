aa1b23fbc6556edd11d19c94139e20a43a580e8c
johnmacha <jontehmacha@gmail.com>
2026-03-25 13:23:35 +0300

    chore: implemented JPA to connect with database

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -11,8 +11,10 @@ import jakarta.persistence.Transient;
 @Entity
 @Table(name = "student")
 public class Student {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id                         
+    @GeneratedValue(
+        strategy = GenerationType.IDENTITY
+    )
     
     private Long id;
     private String name;
