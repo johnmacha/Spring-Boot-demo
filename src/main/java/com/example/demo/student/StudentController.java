173d15e62459e9c9f69c850da5d2ac853f3c0ac2
johnmacha <jontehmacha@gmail.com>
2026-03-26 19:09:45 +0300

    fix: fixed catch error message and added PUT functionality

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -3,13 +3,16 @@ package com.example.demo.student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
@@ -29,13 +32,36 @@ public List<Student> getStudents(){
 }
 
 @PostMapping
-public void registerStudent(@RequestBody Student student){
+public ResponseEntity<?> registerStudent(@RequestBody Student student){
+    try{
     studentService.addStudent(student); //call service
+    return ResponseEntity.ok("Student added successfully");
+    }
+    catch (IllegalStateException e){
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
+
+@PutMapping(path="{id}")
+public void updateStudent(
+    @PathVariable("id") Long id,
+    @RequestParam(required = false) String name,
+    @RequestParam(required = false) String email
+){
+    studentService.updateStudent(id, name, email);
 }
 
 @DeleteMapping("{id}")
-public void deleteStudent(@PathVariable Long id){
-    studentService.deleteStudent(id);
+public ResponseEntity<?> deleteStudent(@PathVariable Long id){
+    try{
+        studentService.deleteStudent(id);
+        return ResponseEntity.ok("Deleted successfully");
+    }
+    catch(IllegalStateException e){
+        // e - is the exception you threw in the service
+        // getMessage() - returns the string you passed to it
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
 }
 
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,10 +1,13 @@
 package com.example.demo.student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 @Service
 public class StudentService {
 	private final StudentRepository studentRepository;
@@ -17,22 +20,53 @@ public class StudentService {
 		return studentRepository.findAll();	
 	}
 
+	@Transactional
 	public void addStudent(Student student){
-		
 		Optional<Student> studentOptional = 
-		studentRepository.findStudentByEmail(student.getEmail());
+		studentRepository.findStudentByEmail(student.getEmail().trim());
 		
-		if(studentOptional.isPresent()){
-			throw new IllegalStateException("email taken");	
+		if(studentOptional.isPresent()){ 
+			//Triggers rollback
+			throw new IllegalStateException("Email "+student.getEmail()+" is already taken");	
 		}
-		studentRepository.save(student);
+		studentRepository.save(student);//Committed if no exception
 	}
+
+	@Transactional
+	public void updateStudent(
+		Long id,
+		String name,
+		String email){
+	Student student = studentRepository.findById(id)
+	.orElseThrow(()-> new IllegalStateException(
+		"student with id" + id + "does not exist"));
+		
+		if(name != null &&
+			name.length() > 0 &&
+			 !Objects.equals(student.getName(), name)){
+			student.setName(name);
+		}
+		
+		if(email != null &&
+			email.length() > 0 &&
+			 !Objects.equals(student.getEmail(), email)){
+			Optional<Student> studentOptional = studentRepository
+			.findStudentByEmail(email);
+
+		if(studentOptional.isPresent()){
+		throw new IllegalStateException("email taken");
+		}
+		} 
+		student.setEmail(email);
+	}
+	 
+	
 
 	public void deleteStudent(Long id){
 		boolean exists = studentRepository.existsById(id);
 		if(!exists) {
 			throw new IllegalStateException(
-				"student with id" + id + "does not exist"
+				"student with id" + id + " does not exist"
 			);
 			}
 		studentRepository.deleteById(id);
